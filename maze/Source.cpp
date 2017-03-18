133af4f76ba61cd5d67c520592de08de505466db
roijjj <eddyyozombie@gmail.com>
2017-03-17 19:41:02 -0700

    Update Source.cpp

--- maze/Source.cpp
+++ maze/Source.cpp
@@ -6,53 +6,9 @@
 #include "level.h"
 level Level;
 
-void game (int lvl) {
 
-	//plaer.init()
-	//l=lvl;
-
-	
-		//Level.move(0, -1);
-
-	Level.load(lvl);
-	Level.print();
-
-	system("pause");
-}
-void move () {
-
-	cout << "to move it is w, s, a, d" << endl;
-
-
-	char input;
-	input = _getch();
-	//cout << input << endl;
-	//Level.move(input);
-	switch (input) {
-
-	case 'w':
-	case 'W':
-		Level.move(0,-1);
-
-		break;
-	case 's':
-	case 'S':
-		Level.move(0,1);
-
-		break;
-	case 'a':
-	case 'A':
-		Level.move(-1,0);
-
-		break;
-	case 'd':
-	case 'D':
-		Level.move(1,0);
-
-		break;
-	}
-
-}
+void move();
+void game(int); 
 void play() {
 	bool flag = false;
 	while (flag != true) {
@@ -69,23 +25,74 @@ int main() {
 	cout << "1. new game \n" << "2. select a level\n" << "3. exit" << endl;
 	int im;
 		 cin>>im;
-
+		
+		 //
 	switch (im) {
-	case 1:
+	case 1://loads game from th first level
+		
 		game (1);
 	play();
 		break;
-	case 2:
+	case 2://loads game from a spcific level
 		int ll;
-		cout << "select a level 1-10";
+		cout << "select a level 1-5";
 		cin >> ll;
-		cout << ll << endl;
-		system("pause");
+		
 		game(ll);
-		play();
+		//play();
 		break;
-	case 3:
+	case 3://exits the game
+		return 0;
 		break;
 	}
 		
+
+}
+void move() {
+
+
+
+	char input;
+	input = _getch();
+
+	switch (input) {
+
+	case 'w':
+	case 'W':
+		Level.move(0, -1);
+
+		break;
+	case 's':
+	case 'S':
+		Level.move(0, 1);
+
+		break;
+	case 'a':
+	case 'A':
+		Level.move(-1, 0);
+
+		break;
+	case 'd':
+	case 'D':
+		Level.move(1, 0);
+
+		break;
+	}
+
+}
+void game (int lvl) {
+
+	//plaer.init()
+	//l=lvl;
+
+
+	//Level.move(0, -1);
+
+	Level.load(lvl);
+	/*bool flag = false;
+	while (flag != true) {
+	Level.print();
+	move();
+	}*/
+
 }
