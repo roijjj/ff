00656c429da71a97784129b52c0b10bcf2d55283
roijjj <eddyyozombie@gmail.com>
2017-03-17 19:42:51 -0700

    Update level.h

--- maze/level.h
+++ maze/level.h
@@ -11,25 +11,45 @@ class level
 {
 public:
 	level();
-	~level();
 	
-	void load(int);
-	void print();
-	void move(int, int );
-	char gett(int, int);
-	void sett(int, int, char);
-	void setposition(int x, int y);
-	void getposition(int& x, int& y);
-	void next();
-	//void relMove(int dx, int dy);
+	
+	void load(int);//loads the level
+	void print();//prints the level
+	void move(int, int );//moves the player
+	void next();//moves to the next leve
 
+	//gets posiont of x and y
+	char gett(int x, int y)
+	{
+		return lvl[y][x];
 
+	}
+	//sets ths postition on x and y
+	void sett(int x, int y, char tile)
+	{
+		lvl[y][x] = tile;
+
+	}
+	//sets the position the player
+	void setposition(int x, int y) {
+		_x = x;
+		_y = y;
+	}
+	//gets the position of the player
+	void getposition(int& x, int& y) {
+		x = _x;
+		y = _y;
+	}
+	
 private:
-	vector <string> lvl;
+	vector <string> lvl;//the level 
+	
 	int _x;
 	int _y;
+	
 	char mt;
 	int l;
 };
+
 
 
